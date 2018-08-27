e3b0d7ac07cb64ed0a3b87933c1b465b8a4fa7bb
Zach <zachcanada23@gmail.com>
2018-08-27 09:30:29 -0500

    Added truck

--- Exercises/src/Chapter1exercises/TablesAndChairs.java
+++ Exercises/src/Chapter1exercises/TablesAndChairs.java
@@ -4,6 +4,15 @@ public class TablesAndChairs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		System.out.println("X                    X");
+		System.out.println("X                    X");
+		System.out.println("X                    X");
+		System.out.println("X     XXXXXXXXXX     X");
+		System.out.println("XXXX   X      X   XXXX");
+		System.out.println("X  X   X      X   X  X");
+		System.out.println("X  X   X      X   X  X");
 
 	}
 
