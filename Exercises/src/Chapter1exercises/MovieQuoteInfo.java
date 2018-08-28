2d117aa351750feaa726b39c99f7c5a4517010a4
Zach <zachcanada23@gmail.com>
2018-08-28 08:52:06 -0500

    added movie quote

--- Exercises/src/Chapter1exercises/MovieQuoteInfo.java
+++ Exercises/src/Chapter1exercises/MovieQuoteInfo.java
@@ -4,7 +4,9 @@ public class MovieQuoteInfo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("");
+		System.out.println("Black Panther");
+		System.out.println("T'Challa says, The wise builds bridges while the foolish builds barriers.");
+		System.out.println("February 16, 2018"); 
 
 	}
 
