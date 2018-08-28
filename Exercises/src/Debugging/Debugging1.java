f0a424803eb2de48edb39f99fb3cbb2580e51868
Zach <zachcanada23@gmail.com>
2018-08-28 08:57:29 -0500

    Debugging 1

---
+++ Exercises/src/Debugging/Debugging1.java
@@ -0,0 +1,11 @@
+package Debugging;
+
+public class Debugging1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		 System.out.println("Hello");
+	}
+
+}
---
+++ Exercises/src/Debugging/Debugging2.java
@@ -0,0 +1,10 @@
+package Debugging;
+
+public class Debugging2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
