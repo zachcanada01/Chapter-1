2fef4c5fa90e5792302a7317e4573e71a9a64c37
Zach <zachcanada23@gmail.com>
2018-08-28 09:30:08 -0500

    Random Guess

---
+++ Exercises/src/RandomGuess.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class RandomGuess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,"The number is "+(1+(int)(Math.random() * 10)));
+
+	}
+
+}
--- Exercises/src/SammysSeashore/SammysMotto.java
+++ Exercises/src/SammysSeashore/SammysMotto.java
@@ -4,6 +4,7 @@ public class SammysMotto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Sammy's makes it fun in the sun.");
 
 	}
 
