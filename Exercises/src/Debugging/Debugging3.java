d19c6e082ed75a56c63599e838c747601173c423
Zach <zachcanada23@gmail.com>
2018-08-28 09:03:59 -0500

    Debugging 3

---
+++ Exercises/src/Debugging/Debugging3.java
@@ -0,0 +1,13 @@
+package Debugging;
+
+public class Debugging3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	      System.out.println("Over the river");
+	      System.out.println("and through the woods");
+	      System.out.println("to Grandmother's house we go");
+	
+		}
+}
