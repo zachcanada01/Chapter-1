64aa396e828ff6bc9354c9694ccc43e21371cb72
Zach <zachcanada23@gmail.com>
2018-08-28 09:19:40 -0500

    Sammy's Motto 2

---
+++ Exercises/src/SammysSeashore/SammysMotto2.java
@@ -0,0 +1,15 @@
+package SammysSeashore;
+
+public class SammysMotto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+		System.out.println("S                              S");
+		System.out.println("S Sammy's makes fun in the sun S");
+		System.out.println("S                              S");
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+
+	}
+
+}
