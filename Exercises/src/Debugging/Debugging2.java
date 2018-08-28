e95f922990d8d0f453d71bac5972dbf7b30abc9c
Zach <zachcanada23@gmail.com>
2018-08-28 08:59:36 -0500

    Debugging 2

--- Exercises/src/Debugging/Debugging2.java
+++ Exercises/src/Debugging/Debugging2.java
@@ -4,6 +4,11 @@ public class Debugging2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Java programming is fun.");
+	      System.out.println("Getting a program to work");
+	      System.out.println("can be a challenge,");
+	      System.out.println("but when everything works correctly,");
+	      System.out.println("it's very satisfying");
 
 	}
 
