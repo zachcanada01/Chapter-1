90eb1cfaf1dbcee9bdd7e145f9a278dbfacb91d7
Zach <zachcanada23@gmail.com>
2018-08-28 08:44:55 -0500

    Added lyrics

--- Exercises/src/Chapter1exercises/FavoriteSong.java
+++ Exercises/src/Chapter1exercises/FavoriteSong.java
@@ -4,6 +4,10 @@ public class FavoriteSong {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("I did not believe that it would end,");
+		System.out.println("Everything came second to the Benzo");
+		System.out.println("You're not even speaking to my friends,");
+		System.out.println("You knew all my uncles and my aunts though");
 
 	}
 
