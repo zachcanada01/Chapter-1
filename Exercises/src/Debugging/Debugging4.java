2be2a942a97d7055803dca1fbc405f9b3b364c21
Zach <zachcanada23@gmail.com>
2018-08-28 09:10:58 -0500

    Debugging 4

---
+++ Exercises/src/Debugging/Debugging4.java
@@ -0,0 +1,10 @@
+package Debugging;
+import javax.swing.JOptionPane;
+public class Debugging4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "First GUI program");
+	}
+
+}
