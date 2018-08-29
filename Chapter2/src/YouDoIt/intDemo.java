32cab7739f886bf0b88fe46fa77373962005ac3b
Zach <zachcanada23@gmail.com>
2018-08-29 09:40:11 -0500

    Updated intDemo

--- Chapter2/src/YouDoIt/intDemo.java
+++ Chapter2/src/YouDoIt/intDemo.java
@@ -4,15 +4,17 @@ public class intDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int anInt = 12;
+		int anInt = 1234;
 		byte aByte = 12;
 		short aShort = 12;
-		long aLong = 12;
+		long aLong = 122451737091237429L;
+		int anotherInt = anInt * 10000000;
 		
 		System.out.println("The int is " + anInt);
 		System.out.println("The byte is " + aByte);
 		System.out.println("The short is " + aShort);
 		System.out.println("The long is " + aLong);
+		System.out.println("Another int is " + anotherInt);
 
 	}
 
