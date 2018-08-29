79a0096c81592f088a3ef95d9ae48a58d1b64b9b
Zach <zachcanada23@gmail.com>
2018-08-29 09:30:34 -0500

    You Do It

---
+++ Chapter2/src/YouDoIt/intDemo.java
@@ -0,0 +1,19 @@
+package YouDoIt;
+
+public class intDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int anInt = 12;
+		byte aByte = 12;
+		short aShort = 12;
+		long aLong = 12;
+		
+		System.out.println("The int is " + anInt);
+		System.out.println("The byte is " + aByte);
+		System.out.println("The short is " + aShort);
+		System.out.println("The long is " + aLong);
+
+	}
+
+}
