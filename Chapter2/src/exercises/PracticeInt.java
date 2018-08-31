5ada0a5dbba32c4de72b076ad5c4aee205b94c29
Zach <zachcanada23@gmail.com>
2018-08-31 08:49:51 -0500

    Added practive intergers

--- Chapter2/src/exercises/PracticeInt.java
+++ Chapter2/src/exercises/PracticeInt.java
@@ -1,10 +1,37 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class PracticeInt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int Num1 ;
+		int Num2;
+		int Total;
+		String name;
+		
+		Scanner input = new Scanner(System.in);
+	
+		
+		System.out.println("What is your name >> ");
+		name = input.next();
+		System.out.println(" Please enter a number >> ");
+		Num1 = input.nextInt();
+		System.out.println(" Please enter another number >> ");
+		Num2 = input.nextInt();
+		
+		Total = Num1 + Num2;
+	
+		 
+		System.out.println("Your name is " + name) ;
+		System.out.println("The total is " + Total );
+	
+		
 
+		 
+		
+	
 	}
 
 }
