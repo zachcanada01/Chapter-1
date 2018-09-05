0735ff237846a43e3788ed4ce28efdeadef1a2cd
Zach <zachcanada23@gmail.com>
2018-09-05 09:35:36 -0500

    NauticalMiles

---
+++ Chapter2/src/exercises/NauticalMiles.java
@@ -0,0 +1,16 @@
+package exercises;
+
+public class NauticalMiles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double kilometers = 1.852;
+		double miles = 1.150779;
+		double nauticalmiles = 1;
+		
+		System.out.println("There are " + kilometers + " kilometers in " + nauticalmiles + " nautical mile");
+		System.out.println("There are " + miles + " miles in " + nauticalmiles + " nautical mile");
+	}
+
+}
