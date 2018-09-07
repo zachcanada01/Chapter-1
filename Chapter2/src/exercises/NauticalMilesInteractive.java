40cbf9d0c6dd441c06c9a7df9db5254b1c269e2c
Zach <zachcanada23@gmail.com>
2018-09-07 08:59:54 -0500

    Nautical Miles

---
+++ Chapter2/src/exercises/NauticalMilesInteractive.java
@@ -0,0 +1,24 @@
+package exercises;
+import java.util.Scanner;
+
+public class NauticalMilesInteractive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double kilometers = 1.852;
+		double miles = 1.150779;
+		double nauticalmiles = 1;
+		
+		miles = (miles * nauticalmiles);
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("How many nautical miles?");
+		nauticalmiles = input.nextInt();
+		
+		System.out.println("This will equal" + miles * nauticalmiles + "miles");
+
+	}
+
+}
